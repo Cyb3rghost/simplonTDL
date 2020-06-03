d45722ccdf17faa35c503836a98565af03eae6a8
Ludovic LEVENEUR <cyb3rghostx@gmail.com>
2020-06-03 13:48:20 +0400

    V8 - Integration script valide.php

--- dashboard.php
+++ dashboard.php
@@ -14,6 +14,11 @@ if(!empty($_SESSION['id']) && !empty($_SESSION['pseudo']))
 	    <!-- CSS only -->
 	    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 	    <link rel="stylesheet" href="CSS/style.css">
+	    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+	    <link rel="stylesheet" href="CSS/style.css">
+    <!-- JS, Popper.js, and jQuery -->
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <script src="https://kit.fontawesome.com/6bffc813f0.js" crossorigin="anonymous"></script>
 	    <!-- JS, Popper.js, and jQuery -->
 	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 	    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
@@ -24,41 +29,73 @@ if(!empty($_SESSION['id']) && !empty($_SESSION['pseudo']))
 		<?php
 			echo "<div style='text-align: center'>Bienvenue, ".$_SESSION['pseudo']." sur votre espace personnel. <a href='scripts/deconnexion.php'>Déconnexion</a></div>";
 		?>
-	<h1 style="text-align: center">Voici votre TODO List </h1>
+<div class="flex">
+<img src="image/d739fc1c-f8f3-4e8f-8f02-d5ca0da3c533_200x200%20(1).png" alt="">
+</div>
+<!-- La modal pour ajouter un TODO -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Créer un TODO</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <textarea class="form-control tache" id="exampleFormControlTextarea1" rows="3" placeholder="..."></textarea>
+            </div>
 
-	<!-- La modal pour ajouter un TODO -->
-	<!-- Modal -->
-	<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-	    <div class="modal-dialog" role="document">
-	        <div class="modal-content">
-	            <div class="modal-header">
-	                <h5 class="modal-title" id="exampleModalLabel">Ajouter une tâche</h5>
-	                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-	                    <span aria-hidden="true">&times;</span>
-	                </button>
-	            </div>
-	            <div class="modal-body">
-	                <input type="text" class="form-control" aria-label="Text input with checkbox" placeholder="TODO 1">
-	            </div>
-	            <div class="modal-footer">
-	                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
-	                <button type="button" class="btn btn-primary btn_valider_aj">Ajouter</button>
-	            </div>
-	        </div>
-	    </div>
-	</div>
-	<div class="flex">
 
-	<div class="read">
 
-	    <div style="text-align: center"> <h2>ajouter un TODO  : <button type="button" class="btn btn-primary btn_ajout_t" data-toggle="modal" data-target="#exampleModal">ajouter</button></h2></div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
+                <button type="button" class="btn btn-primary btn_valider_aj">Submit</button>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- la modal pour assigner un todo-->
 
-	    <ul class='liste_tache'>
-	        <!-- Zone d'affichage de la liste des tâches -->
-	    </ul>
-	</div>
-	</div>
+<div class="modal fade" id="exampleModal1" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel1" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel1">Attribuer un TODO</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="input-group mb-3">
 
+                <select class="custom-select" id="liste_u">
+                    <option selected>Attribuer à un utilisateur</option>
+                    <!-- Zone d'affichage de la liste des utilisateur -->
+                </select>
+                </div>
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
+                <button type="button" class="btn btn-primary">Submit</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="flex">
+
+<div class="read">
+
+    <div style="text-align: center"> <h2>ajouter un TODO  : <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">ajouter</button></h2></div>
+<div class="flex">
+    <ul class='liste_tache' style="  list-style-type: none">
+    	<!-- Zone d'affichage de la liste des tâches -->
+    </ul>
+</div>
+</div>
 	</body>
 	</html>
 <?php
--- scripts/server.php
+++ scripts/server.php
@@ -3,17 +3,16 @@
 		//	Connexion de l'utiisateur
 		require('connexion.php');
 	}elseif(isset($_POST['action']) || isset($_POST['action'])){
-		$id_prop = isset($_POST['id_prop'])?$_POST['id_prop']:$id_prop;
+		$id_prop = isset($_POST['id_prop'])?$_POST['id_prop']:'';
 		$action = isset($_POST['action'])?$_POST['action']:$action;		
 		if($action == 'liste_user'){
 			//	Récupération de la liste des utilisateurs
-			require('listetaches.php');
 			//echo json_encode($liste_user);
-		}elseif($action == 'voir'){			
-			if(isset($_POST['id_user'])){
+		}elseif($action == 'voir'){		
 				//	Récupération de la liste des tâches
-
-				//echo json_encode($data);
+				require('listetaches.php');	
+			if($erreur == 0){
+				echo json_encode($arr_res);	//	Variable définie dans le fichier "listetaches.php";
 			}else{
 				echo json_encode(array(array('id' => 'erreur')));
 			}
