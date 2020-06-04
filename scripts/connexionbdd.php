afbb57f16681d907cadd02f05bf261ab877ae8b1
Sauvagecyprien <cyprien.sauvage974@gmail.com>
2020-06-04 15:40:00 +0400

    commentaires dans le code des scripts 4.5.6

--- scripts/connexionbdd.php
+++ scripts/connexionbdd.php
@@ -1,6 +1,6 @@
 <?php 
-
-$pdo = new PDO('sqlite:../simplonn.db');
+//fichier de connexion à la base de donnée //
+$pdo = new PDO('sqlite:../simplonn.db'); // on donne le chemin d'accès //
 $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // ERRMODE_WARNING | ERRMODE_EXCEPTION | ERRMODE_SILENT
 
