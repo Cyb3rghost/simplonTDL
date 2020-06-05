f17d5c837628c027352d65d5ecb989692291273a
Cédrick BARO <mrcedrickmichelpro@gmail.com>
2020-06-05 08:46:53 +0400

    Mise à jour Final

--- scripts/listeutilisateursattribuer.php
+++ scripts/listeutilisateursattribuer.php
@@ -6,22 +6,17 @@ if(!empty($_SESSION['id']) && !empty($_SESSION['pseudo']) && isset($_POST['idtac
     $idTache = $_POST['idtache'];
 
     //inclue fichier de connection à la base de données
-    include('connexionbdd.php');
-    
-<<<<<<< HEAD
+    include('connexionbdd.php'); 
+
+
+    //préparation de la requete
     $stmt = $pdo->prepare("SELECT users.nom as nom, affecter.id as id_affectation FROM affecter INNER JOIN users ON users.id = affecter.user_id WHERE tache_id = :idtache AND affecter.user_id != :id_user");
     $stmt->bindValue(':idtache', $idTache);
     $stmt->bindValue(':id_user', $_SESSION['id']);
-    $stmt->execute();
-    
-=======
-    //préparation de la requete
-    $stmt = $pdo->prepare("SELECT users.nom as nom, affecter.id as id_affectation FROM affecter INNER JOIN users ON users.id = affecter.user_id WHERE tache_id = :idtache");
-    $stmt->bindValue(':idtache', $idTache);
     //execution de la requete
     $stmt->execute();
     //creation d'un tableau qui recuperera les données la requete
->>>>>>> d664a61d834b8cb6ffedaa8bca9b1dcb91730f67
+
     $arr_res = array();
     $erreur = 0;    //  Variable qui sera exploité du côté du fichier server.php pour savoir s'il y eu une erreur ou pas (valeur 0 indique pas d'erreur et 1 indiqu'il y a eu d'erreur)
     //insertion des données dans le tableau
